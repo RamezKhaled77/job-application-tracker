275c3f3d433f874d41b3fee3f6bc265f08fc6e11
Ramez Khaled <ramez.elzoz74@gmail.com>
2026-08-28 23:27:42 +0300

    UI: creating the sign-up, sign-in ui

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,3 +1,77 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 export default function SignInPage() {
-  return <div>SignIn</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
+      <Card className="w-full max-w-md border-gray-200 shadow-lg">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold text-black">
+            Log In
+          </CardTitle>
+          <CardDescription className="text-gray-600">
+            Log in to access your account
+          </CardDescription>
+        </CardHeader>
+        <form className="space-y-4">
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email" className="text-gray-700">
+                Email
+              </Label>
+              <Input
+                className="border-gray-300 h-9 focus:border-primary focus:ring-primary"
+                type="email"
+                id="email"
+                placeholder="ahmed-ali@exmaple.com"
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password" className="text-gray-700">
+                Password
+              </Label>
+              <Input
+                className="border-gray-300 h-9 focus:border-primary focus:ring-primary"
+                type="password"
+                id="password"
+                minLength={8}
+                placeholder="********"
+                required
+              />
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-4">
+            <Button
+              type="submit"
+              className="w-full bg-primary hover:bg-primary/90 cursor-pointer h-10"
+            >
+              Sign in
+            </Button>
+            <p className="text-center text-sm text-gray-600">
+              Don&apos;t have an account?
+              <Link
+                href="/sign-up"
+                className="font-medium text-primary hover:underline"
+              >
+                {" "}
+                Sign Up
+              </Link>
+            </p>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
 }
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,3 +1,89 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 export default function SignUpPage() {
-  return <div>SignUp</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
+      <Card className="w-full max-w-md border-gray-200 shadow-lg">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold text-black">
+            Sign Up
+          </CardTitle>
+          <CardDescription className="text-gray-600">
+            Create an account to start tracking your job applications
+          </CardDescription>
+        </CardHeader>
+        <form className="space-y-4">
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="name" className="text-gray-700">
+                Name
+              </Label>
+              <Input
+                className="border-gray-300 h-9 focus:border-primary focus:ring-primary"
+                type="text"
+                id="name"
+                placeholder="Ahmed Ali"
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email" className="text-gray-700">
+                Email
+              </Label>
+              <Input
+                className="border-gray-300 h-9 focus:border-primary focus:ring-primary"
+                type="email"
+                id="email"
+                placeholder="ahmed-ali@exmaple.com"
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password" className="text-gray-700">
+                Password
+              </Label>
+              <Input
+                className="border-gray-300 h-9 focus:border-primary focus:ring-primary"
+                type="password"
+                id="password"
+                minLength={8}
+                placeholder="********"
+                required
+              />
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-4">
+            <Button
+              type="submit"
+              className="w-full bg-primary hover:bg-primary/90 cursor-pointer h-10"
+            >
+              Sign up
+            </Button>
+            <p className="text-center text-sm text-gray-600">
+              Already have an account?
+              <Link
+                href="/sign-in"
+                className="font-medium text-primary hover:underline"
+              >
+                {" "}
+                Sign In
+              </Link>
+            </p>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
 }
