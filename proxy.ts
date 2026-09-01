7d49d81ed2e14b7778d2ac81a25e90d45a2ae4ad
Ramez Khaled <ramez.elzoz74@gmail.com>
2026-09-01 18:49:01 +0300

    implement the roxy file

---
+++ proxy.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "./lib/auth/auth";
+
+export default async function proxy(req: NextRequest) {
+  const session = await getSession();
+
+  const isDashboardPage = req.nextUrl.pathname.startsWith("/dashboard");
+
+  if (isDashboardPage && !session?.user) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+
+  const isSignInPage = req.nextUrl.pathname.startsWith("/sign-in");
+  const isSignUpPage = req.nextUrl.pathname.startsWith("/sign-up");
+
+  if ((isSignInPage || isSignUpPage) && session?.user) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  return NextResponse.next();
+}
